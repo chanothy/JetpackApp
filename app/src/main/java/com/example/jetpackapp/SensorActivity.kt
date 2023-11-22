df436cba4883dc0c316978c2f38d95b3ac0bb27c
timchan <timchan@iu.edu>
2023-11-22 12:19:07 -0500

    Permission and Location

--- app/src/main/java/com/example/jetpackapp/SensorActivity.kt
+++ app/src/main/java/com/example/jetpackapp/SensorActivity.kt
@@ -1,51 +1,100 @@
 package com.example.jetpackapp
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresPermission
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.jetpackapp.ui.theme.JetpackAppTheme
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationTokenSource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            JetpackAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-//                    Greeting("Tim")
-                    ShowInfo("Tim", "Bloomington, Indiana", "45")
-                }
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    Log.d("Permissions","")
+                    setContent {
+                        JetpackAppTheme {
+
+                            // A surface container using the 'background' color from the theme
+                            Surface(
+                                modifier = Modifier.fillMaxSize(),
+                                color = MaterialTheme.colorScheme.background
+                            ) {
+//                                ShowInfo("Tim", "Bloomington, Indiana", "45")
+                                if (ActivityCompat.checkSelfPermission(
+                                        this,
+                                        Manifest.permission.ACCESS_COARSE_LOCATION
+                                    ) != PackageManager.PERMISSION_GRANTED
+                                ) {
+                                    Log.d("Current location","Permission Not Granted")
+                                } else {
+                                    CurrentLocationContent(false)
+                                    Log.d("Current location","Permission Granted")
+                                }
+                            }
+                        }
+                    }
+                } else -> {
+                // No location access granted.
+            }
             }
         }
+
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION))
+
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
+
+
+
+
 
 @Composable
 fun ShowInfo(name: String, location: String, temperature: String) {
@@ -64,10 +113,88 @@ fun ShowInfo(name: String, location: String, temperature: String) {
     }
 }
 
+
+
+@RequiresPermission(
+    anyOf = [Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION],
+)
+
+
+
+@Composable
+fun CurrentLocationContent(usePreciseLocation: Boolean) {
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+    val locationClient = remember {
+        LocationServices.getFusedLocationProviderClient(context)
+    }
+    var locationInfo by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .animateContentSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Button(
+            onClick = {
+                // getting last known location is faster and minimizes battery usage
+                // This information may be out of date.
+                // Location may be null as previously no client has access location
+                // or location turned of in device setting.
+                // Please handle for null case as well as additional check can be added before using the method
+                scope.launch(Dispatchers.IO) {
+                    val result = locationClient.lastLocation.await()
+                    locationInfo = if (result == null) {
+                        "No last known location. Try fetching the current location first"
+                    } else {
+                        "Current location is \n" + "lat : ${result.latitude}\n" +
+                                "long : ${result.longitude}\n" + "fetched at ${System.currentTimeMillis()}"
+                    }
+                }
+            },
+        ) {
+            Text("Get last known location")
+        }
+
+        Button(
+            onClick = {
+                //To get more accurate or fresher device location use this method
+                scope.launch(Dispatchers.IO) {
+                    val priority = if (usePreciseLocation) {
+                        Priority.PRIORITY_HIGH_ACCURACY
+                    } else {
+                        Priority.PRIORITY_BALANCED_POWER_ACCURACY
+                    }
+                    val result = locationClient.getCurrentLocation(
+                        priority,
+                        CancellationTokenSource().token,
+                    ).await()
+                    result?.let { fetchedLocation ->
+                        locationInfo =
+                            "Current location is \n" + "lat : ${fetchedLocation.latitude}\n" +
+                                    "long : ${fetchedLocation.longitude}\n" + "fetched at ${System.currentTimeMillis()}"
+                    }
+                }
+            },
+        ) {
+            Text(text = "Get current location")
+        }
+        Text(
+            text = locationInfo,
+        )
+    }
+}
+
+// For showing previews
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackAppTheme {
-        Greeting("Android")
     }
 }
+
